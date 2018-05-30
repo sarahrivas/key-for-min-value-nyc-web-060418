b65b44d6fc223e9befc859fadc44921d59c89af5
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-30 04:03:47 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if smallest_value > value
       smallest_value = value
+      key_for_smallest_val = key
     end  
   end  
+  key_for_smallest_val
 end
